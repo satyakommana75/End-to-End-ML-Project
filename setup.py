3af9bd0639b3097f924f57180a6cec359748fb37
satyakommana75 <satyakommana75@gmail.com>
2023-12-30 03:18:46 +0530

    E-mail added satyakommana75@gmail.com

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ __version__ = "0.0.0"
 REPO_NAME = "End-to-end-ML-Project-Implementation"
 AUTHOR_USER_NAME = "entbappy"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "satyakommana75@gmail.com"
 
 
 setuptools.setup(
