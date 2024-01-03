869e7b9973b325bbe8ad29bb45931ac0160b3c64
satyakommana75 <satyakommana75@gmail.com>
2024-01-04 00:06:02 +0530

    add data ingestion

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+## End-to-end-ML-Project
+## Workflows
+## 1.update config.yaml
+## 2.update schema.yaml
+## 3.update params.yaml
+## 4.update the entity
+## 5.update the configuration manager in src config
+## 6.update the components
+## 7.update the pipeline
+## 8.update the main.py
+## 9.update the app.py
+
+## How to run?
+## STEPS:
+## 1.conda create -n mlproj python=3.8 -y 
+## 2.conda activate mlproj
+## 3.pip install -r requirements.txt
+
+
+
 # End-to-End-ML-Project
 ## Bapy-End to End ML Project
 ##Python demp.py 
--- main.py
+++ main.py
@@ -1,3 +1,40 @@
-## from src.mlProject.logging import logger
 from mlProject import logger
-logger.info("This is out Custom LOG")
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
